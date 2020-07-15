1783ac2837aa8153fefc092c65a5749755cdd2ed
Alejandro Gonzalez <sieg_1594@hotmail.com>
2020-07-15 12:40:04 -0400

    FIRST COMMIT LOGIN MODULE
    ez

---
+++ src/server/SocketManager.js
@@ -0,0 +1,22 @@
+const io = require('./index.js')
+
+const connectedUser = {}
+
+module.exports = function(socket){
+    console.log("Socket Id" + socket.id);
+
+
+
+}
+
+function addUser(userList, user){
+
+}
+
+function removeUser(userList, username){
+
+}
+
+function isUser(userList, username){
+
+}
